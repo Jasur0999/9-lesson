4e81d78e090140163298018a3b7e8b122eb4d0d2
Jasur0999 <leaderjasur@gmail.com>
2024-01-15 15:02:39 +0500

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html", './pages/*.html'],
+  theme: {
+    extend: {},
+    screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1240px"
+		}
+
+  },
+  plugins: [],
+}
+
